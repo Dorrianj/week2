e825c22aa42c53df8c057a77f8b8a2276e910306
Dorrianj <52667212+Dorrianj@users.noreply.github.com>
2019-07-17 13:58:25 -0400

    Add files via upload

---
+++ day8.java
@@ -0,0 +1,32 @@
+package day8;
+
+public class fizzbuzz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int x=1; x<= 100; x++){
+			
+			
+		 if( x%3 ==0 && x%5 == 0) {
+				System.out.println("fizz buzz");
+				
+			}else if( x%5 == 0) {
+					System.out.println("buzz");
+					
+			}else if( x%3 == 0) {
+				System.out.println("fizz"); 
+					
+				}
+				System.out.println(x);
+				}
+					
+				}
+				
+				}
+		
+		
+		
+	
+
+
