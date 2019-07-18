9013afd3c601d9a6065383252936a58b0d79e53e
Dorrianj <52667212+Dorrianj@users.noreply.github.com>
2019-07-18 11:36:29 -0400

    Add files via upload

---
+++ morningexercise.java
@@ -0,0 +1,38 @@
+package day9;
+
+import java.util.Scanner;
+
+public class morningexercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		Scanner sncr = new Scanner(System.in);
+		
+//		int bye = 5;
+//		int hi = 10;
+//		int back = 0;
+		
+		System.out.println("welcome to warm-up!");
+		System.out.println("enter a number");
+		int bye= sncr.nextInt();
+		
+//		System.out.println("enter a number");
+//		back= sncr.nextInt();
+		
+		for(int x=bye; x<=100; x++) {
+		
+			
+		if (bye>50) {
+			System.out.println(x);
+			
+		} 
+		}
+	}
+	}
+		
+
+
+	
+
+
